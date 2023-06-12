bbcba5bbb655f3155dcb5d725a18d53d69a563a0
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-06-13 00:06:35 +0530

    13 june

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+burger = document.querySelector('.burger');
+nav = document.querySelector('nav');
+sidebarnav = document.querySelector('.sidebarnav');
+
+
+burger.addEventListener('click',() => {
+    // nav.classList.toggle('heightnav');
+    sidebarnav.classList.toggle('sidebarnavrtpos');
+});
+
+let showMenu = false;
+burger.addEventListener("click", toggleMenu);
+function toggleMenu() {
+    if (!showMenu) {
+        // Reset the menu state
+        showMenu = true;
+    } else {
+        // Reset the menu state
+        showMenu = false;
+    }
+}
